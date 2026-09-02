096bf3844e01c4a109319d76b0e2d7270d2c8481
nataliemonda <nattymonda254@gmail.com>
2026-09-02 13:59:47 +0300

    Update find_and_create_missing_routers.py

--- find_and_create_missing_routers.py
+++ find_and_create_missing_routers.py
@@ -15,8 +15,8 @@ from datetime import datetime
 # ============================================
 
 API_BASE_URL = "https://home.mawingunetworks.com/api/2.0"
-API_KEY = "6b16484d021797bb5be96ffa58ff1a43"        # Replace with your actual key
-API_SECRET = "2727d0bf6b37e520a3275a49dd6d4b6a"  # Replace with your actual secret
+API_KEY = "API Key"        # Replace with your actual key
+API_SECRET = "Secret Key"  # Replace with your actual secret
 
 # Input files
 REGION_MAP_FILE = 'region_map.csv'
