823f04be3a3d66f110116d0d8b598812d5bb1c5a
nataliemonda <nattymonda254@gmail.com>
2026-09-02 13:01:07 +0300

    Update router_contentions.py

--- router_contentions.py
+++ router_contentions.py
@@ -4,8 +4,8 @@ import json
 
 url = "https://home.mawingunetworks.com/api/2.0/admin/networking/monitoring/681"
 
-key = "6b16484d021797bb5be96ffa58ff1a43"
-secret = "e81d934c83ed576f9c2414b923793289"
+key = "API Key"
+secret = "Secret API"
 
 response = requests.get(
     url,
