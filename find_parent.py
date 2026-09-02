b92516a2e8ed316187a103e2660231aaf9e634b5
nataliemonda <nattymonda254@gmail.com>
2026-09-02 13:11:43 +0300

    Update find_parent.py

--- find_parent.py
+++ find_parent.py
@@ -3,8 +3,8 @@ from requests.auth import HTTPBasicAuth
 
 BASE_URL = "https://home.mawingunetworks.com/api/2.0"
 
-KEY = "9687bab6a5e7a924de376513a691f25e"
-SECRET = "c020d5c275b9218c8fdba9ebc1e81f96"
+KEY = "API Key"
+SECRET = "Secret API"
 
 response = requests.get(
     f"{BASE_URL}/admin/networking/monitoring",
