6aa4a89bc6543a08d16cb23cbac6c83082fbac7c
nataliemonda <nattymonda254@gmail.com>
2026-09-02 13:23:37 +0300

    Update create_access_devices.py

--- create_access_devices.py
+++ create_access_devices.py
@@ -15,8 +15,8 @@ from datetime import datetime
 # ============================================
 
 API_BASE_URL = "https://home.mawingunetworks.com/api/2.0"
-API_KEY = "6b16484d021797bb5be96ffa58ff1a43"        # Replace with your actual key
-API_SECRET = "7df14f2cd623063e6c828099a30adf64"  # Replace with your actual secret
+API_KEY = "API Key"        # Replace with your actual key
+API_SECRET = "Secret API"  # Replace with your actual secret
 
 # Input files
 MISSING_DEVICES_FILE = 'missing_monitoring.csv'
